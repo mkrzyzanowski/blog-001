3298b73ad8e0e4d7bbb212018b8581897f13ca32
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2017-10-15 22:10:29 +0200

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+#!groovy
+
+node('master') {
+    stage('Checkout') {
+        checkout scm
+    }
+    
+    stage('Run tests') {
+        try {
+            withMaven(maven: 'Maven 3') {
+                dir('bobcat') {
+                    sh 'mvn clean test -Dwebdriver.type=remote -Dwebdriver.url=http://localhost:4444/wd/hub -Dwebdriver.cap.browserName=chrome -Dmaven.test.failure.ignore=true'
+                }
+            }
+        } finally {
+            junit testResults: 'bobcat/target/*.xml', allowEmptyResults: true
+            archiveArtifacts 'bobcat/target/**'
+        }
+    }
+}
---
+++ bobcat/src/test/java/com/automatingguy/WikipediaTest.java
@@ -0,0 +1,34 @@
+package com.automatingguy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.cognifide.qa.bb.junit.Modules;
+import com.cognifide.qa.bb.junit.TestRunner;
+import com.google.inject.Inject;
+import com.automatingguy.GuiceModule;
+import com.automatingguy.pageobjects.WikipediaPage;
+import com.automatingguy.pageobjects.DefinitionPage;
+
+@RunWith(TestRunner.class)
+@Modules(GuiceModule.class)
+public class WikipediaTest {
+
+  private static final String SEARCH_QUERY = "hello world";
+  private static final String HEADING = "\"Hello, World!\" program";
+
+  @Inject
+  private WikipediaPage homePage;
+
+  @Inject
+  private DefinitionPage definitionPage;
+
+  @Test
+  public void wikipediaSearchTest() {
+    homePage.open().getSearchComponent().searchForQuery(SEARCH_QUERY);
+    assertThat(definitionPage.getHeading(), is(HEADING));
+  }
+}
